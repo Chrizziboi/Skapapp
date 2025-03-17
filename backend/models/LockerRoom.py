575f63644db631e12f519172070c814f1d4d312c
Christopher Fredh <88053773+Chrizziboi@users.noreply.github.com>
2025-03-17 15:53:56 +0100

    restored the old format, and changed some inconsitensies.
    made a few changes to certain functions that made no sense and now are more in line with SQLAlchemy.

--- backend/models/LockerRoom.py
+++ backend/models/LockerRoom.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import relationship, Session
 from database import Base
 from fastapi import HTTPException
 
-class locker_room(Base):
+class LockerRoom(Base):
     __tablename__ = "locker_rooms"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+    location = Column(String, default="None", index=True)
 
     lockers = relationship("Locker", back_populates="locker_room")
 
@@ -17,12 +18,12 @@ def create_locker_room(name: str, db: Session):
     Oppretter et nytt garderoberom hvis det ikke allerede finnes.
     """
     # Sjekk om garderoberommet allerede finnes
-    existing_room = db.query(locker_room).filter(locker_room.name == name).first()
+    existing_room = db.query(LockerRoom).filter(LockerRoom.name == name).first()
     if existing_room:
         raise HTTPException(status_code=400, detail=f"Garderoberom med navn '{name}' Finnes allerede.")
 
     # Opprett og legg til et nytt garderoberom
-    new_locker_room = locker_room(name=name)
+    new_locker_room = LockerRoom(name=name)
     db.add(new_locker_room)
     db.commit()
     db.refresh(new_locker_room)
